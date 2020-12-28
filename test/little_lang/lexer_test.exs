6701632250d531853fdd989de813e12b279c48e9
JohnnyT <ubergeek3141@gmail.com>
2020-12-28 14:49:42 -0700

    Uses process! in tests

--- test/little_lang/lexer_test.exs
+++ test/little_lang/lexer_test.exs
@@ -5,32 +5,35 @@ defmodule LittleLang.LexerTest do
 
   doctest Lexer
 
+  test "process returns wrapped tokens" do
+    assert {:ok, [{:identifier, 1, "true"}]} = Lexer.process("true")
+  end
+
   test "process! returns unwrapped tokens" do
     assert [{:identifier, 1, "true"}] = Lexer.process!("true")
   end
 
   test "lexes and removes whitespace" do
-    assert {:ok, [{:identifier, 1, "true"}]} = Lexer.process(" true")
-    assert {:ok, [{:identifier, 1, "true"}]} = Lexer.process("true\t")
-    assert {:ok, [{:identifier, 1, "true"}]} = Lexer.process("\rtrue\n")
-    assert {:ok, [{:identifier, 1, "true"}]} = Lexer.process("  \t \r\t true\r\n\t  \t")
+    assert [{:identifier, 1, "true"}] = Lexer.process!(" true")
+    assert [{:identifier, 1, "true"}] = Lexer.process!("true\t")
+    assert [{:identifier, 1, "true"}] = Lexer.process!("\rtrue\n")
+    assert [{:identifier, 1, "true"}] = Lexer.process!("  \t \r\t true\r\n\t  \t")
 
-    assert {:ok, [{:identifier, 1, "true"}, {:identifier, 1, "false"}]} =
-             Lexer.process("true\t false")
+    assert [{:identifier, 1, "true"}, {:identifier, 1, "false"}] = Lexer.process!("true\t false")
   end
 
   test "lexes and removes comments" do
-    assert {:ok, [{:identifier, 1, "true"}]} = Lexer.process("true # Single line comment")
+    assert [{:identifier, 1, "true"}] = Lexer.process!("true # Single line comment")
   end
 
   test "lexes identifiers" do
-    assert {:ok, [{:identifier, 1, "true"}]} = Lexer.process("true")
-    assert {:ok, [{:identifier, 1, "false"}]} = Lexer.process("false")
-    assert {:ok, [{:identifier, 1, "undefined"}]} = Lexer.process("undefined")
+    assert [{:identifier, 1, "true"}] = Lexer.process!("true")
+    assert [{:identifier, 1, "false"}] = Lexer.process!("false")
+    assert [{:identifier, 1, "undefined"}] = Lexer.process!("undefined")
 
-    assert {:ok, [{:identifier, 1, "var_1"}]} = Lexer.process("var_1")
-    assert {:ok, [{:identifier, 1, "_var"}]} = Lexer.process("_var")
-    assert {:ok, [{:identifier, 1, "Var_"}]} = Lexer.process("Var_")
+    assert [{:identifier, 1, "var_1"}] = Lexer.process!("var_1")
+    assert [{:identifier, 1, "_var"}] = Lexer.process!("_var")
+    assert [{:identifier, 1, "Var_"}] = Lexer.process!("Var_")
   end
 
   test "does not lex invalid identifiers" do
@@ -38,8 +41,8 @@ defmodule LittleLang.LexerTest do
   end
 
   test "lexes unary operators" do
-    assert {:ok, [{:bang, 1, "!"}]} = Lexer.process("!")
-    assert {:ok, [{:not_, 1, "not"}]} = Lexer.process("not")
-    assert {:ok, [{:bang, 1, "!"}, {:identifier, 1, "true"}]} = Lexer.process("!true")
+    assert [{:bang, 1, "!"}] = Lexer.process!("!")
+    assert [{:not_, 1, "not"}] = Lexer.process!("not")
+    assert [{:bang, 1, "!"}, {:identifier, 1, "true"}] = Lexer.process!("!true")
   end
 end
