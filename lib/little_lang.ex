b57b44dee460e0d008fe3293ad7c06979d637980
JohnnyT <ubergeek3141@gmail.com>
2021-10-26 14:20:25 -0600

    Adds helpers to LittleLang

--- lib/little_lang.ex
+++ lib/little_lang.ex
@@ -7,6 +7,7 @@ defmodule LittleLang do
   alias __MODULE__.Lexer
   alias __MODULE__.Parser
   alias __MODULE__.Visitors.InstructionsVisitor
+  alias __MODULE__.Visitors.StringVisitor
 
   @doc """
   Compiles and evaluates the provided string.
@@ -38,5 +39,36 @@ defmodule LittleLang do
     |> Parser.process!()
     |> InstructionsVisitor.accept()
     |> Evaluator.process(context)
+  end
+
+  @doc """
+  Parses the provided string and returns it formatted
+
+  ## Examples
+
+      iex> LittleLang.as_string("(3    -     4) = -1")
+      "(3 - 4) = -1"
+
+  """
+  def as_string(string) do
+    string
+    |> Lexer.process!()
+    |> Parser.process!()
+    |> StringVisitor.accept()
+  end
+
+  @doc """
+  Parses the provided string and returns the instructions
+
+  ## Examples
+
+      iex> LittleLang.compile("(3 - 4) = -1")
+
+  """
+  def compile(string) do
+    string
+    |> Lexer.process!()
+    |> Parser.process!()
+    |> InstructionsVisitor.accept()
   end
 end
